dcc759aaaed3ef789c495ae8c8a4abffbc5e0e78
T. G. McLean <tg@tgmclean.com>
2023-09-12 06:32:50 -0600

    Simplified hero. Stubbed footer.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 
 export default function Page() {
   return (
     <>
-      <Header />
       <Hero />
+      <Footer />
     </>
   );
 }
