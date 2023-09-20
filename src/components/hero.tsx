6679a4e5554981e936282e7f044747e437ed5c7e
T. G. McLean <tg@tgmclean.com>
2023-09-20 15:14:42 -0600

    Logo on-screen.

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,26 @@
+import Image from "next/image";
+
 const Hero: any = () => {
   return (
     <div className="hero h-5/6 bg-base-200">
       <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Studio Kookajoo</h1>
+        <div className="max-w-2xl">
+          {/* <img
+            className="w-32 md:w-32 lg:w-48"
+            src="/kookajoo-logo.svg"
+            alt="Studio Kookajoo logo"
+          /> */}
+          <Image
+            src="/kookajoo-logo.svg"
+            alt="Studio Kookajoo logo"
+            width={2500}
+            height={750}
+            // fill
+            // sizes="(max-width: 768px) 10w, (max-width: 1200px) 5w, 16hw"
+            // sizes="w-16"
+            // className="w-16 md:w-32 lg:w-48"
+            // className="dark:invert"
+          />
         </div>
       </div>
     </div>
