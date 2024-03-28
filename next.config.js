48eaee1d2afb85d20f71b9b83c1278c8b457277a
T. G. McLean <tg@tgmclean.com>
2024-03-28 09:24:51 -0600

    Commented out basePath.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/kookajoo.com",
+    // basePath: "/kookajoo.com",
     output: "export",
     reactStrictMode: true,
 };
