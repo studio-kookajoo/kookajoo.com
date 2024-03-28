9506783be5a42879e5d5f5d128c5e9c0e59f717b
T. G. McLean <tg@tgmclean.com>
2024-03-28 09:40:57 -0600

    nextConfig changes.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // basePath: "/kookajoo.com",
     output: "export",
     reactStrictMode: true,
+    images: {
+        loader: 'akamai',
+        path: '',
+    },
+    assetPrefix: './',
 };
 
 module.exports = nextConfig
