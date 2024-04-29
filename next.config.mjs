56537f1987421117bc8be6170b968183d77c7b42
T. G. McLean <tg@tgmclean.com>
2024-04-29 06:06:56 -0600

    next.config clean-up.

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === "production";
-
 const repo = "kookajoo.com";
-// const assetPrefix = `/${repo}`;
-// const basePath = `/${repo}`;
 
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: isProd ? `https://${repo}` : undefined,
-//   basePath: basePath,
   output: "export",
   distDir: "docs",
 };
