9af55d6626adc4bf4dee95f0542ffc65a68e4650
T. G. McLean <tg@tgmclean.com>
2024-09-27 11:44:50 -0600

    Nuke.

--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header: any = () => {
-  return (
-    <div className="navbar bg-base-100">
-      <a className="btn btn-ghost normal-case text-xl">Studio Kookajoo</a>
-    </div>
-  );
-};
-
-export default Header;
